4657592e5f5572c2aa3512bf8a78c96f27ece31d
Camille Dbschr <c.debusscher@gmail.com>
2022-11-09 08:40:59 +0100

    add(template): legals page

---
+++ app/Views/frontend/legals/mentionslegales.php
@@ -0,0 +1,3 @@
+<?php $title = "Mentions Légales"; $description = "Best Music, le vendeur numéro 1 de Vinyles"; ob_start() ?>
+
+<?php $content = ob_get_clean(); require_once 'app/Views/frontend/templates/template.php' ?>
---
+++ app/Views/frontend/legals/rgpd.php
@@ -0,0 +1,3 @@
+<?php $title = "RGPD"; $description = "Best Music, le vendeur numéro 1 de Vinyles"; ob_start() ?>
+
+<?php $content = ob_get_clean(); require_once 'app/Views/frontend/templates/template.php' ?>
